45e674424e5c74fec8cdd08aa84c7a8b0e8088ed
mac <aelkhazenti@gmail.com>
2020-03-25 14:56:29 +0100

    admition en dut

--- 404.php
+++ 404.php
@@ -1,33 +1,100 @@
 <?php get_header(); ?>
 
 
-    <div class="w3-w3-col l8 s12">
+    <body class="page-template-default page page-id-1728 page-child parent-pageid-1595" data-title="Les DUT">
 
 
-        <div class="w3-card-4 w3-margin white">
+            <div id="main-content" class="site-content">
+                <div class="breadcrumb-wrapper">
+                    <a class="toggle" href="javascript:toggleBreadcrumb()"><i aria-hidden="true" class="far fa-sitemap"></i> Fil d'ariane</a>
+                    <div class="breadcrumb-wrapper-content">
+
+                    </div>
+                </div>
+                <div class="page-wrapper with-sidebar">
+                    <div id="sidebar" class="page-navigation">
+                        <div class="page-navigation-wrapper" data-simplebar="init">
+                            <ul class="page-navigation-sections page-navigation-content"></ul>
+                        </div>
+                    </div>
+                    <div class="page-navigation-toggle">
+                        <a href="javascript:pageNavigationToggle()" title="Afficher/Masquer la barre latérale de navigation dans la page"><i aria-hidden="true" class="far fa-chevron-right"></i><i aria-hidden="true" class="far fa-chevron-left" style="display:none"></i></a>
+                    </div>
+                    <div id="sections" class="page-sections">
+                        <section id="x3ACioUOXcsIP34bST2X" class="margin-top-min no-margin-bottom no-padding-top no-padding-bottom border-top-no border-bottom-no bg-white txt-default no-bg-image " style="">
+                            <div id="row-z7o33tkr6yoxGoEoPGLU" class="row row1col row-100 rowclassic no-margin-top no-margin-bottom no-padding-top no-padding-bottom next-to-sidebar ">
+                                <div class="col">
+                                    <div class="bloc bloc-texte">
+                                        <p>Le DUT <strong>(Diplôme Universitaire de Technologie)</strong> est le diplôme national qui représente la plus grande partie des effectifs des IUT. Les IUT proposent actuellement des DUT dans 25 spécialités </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                        <section id="Lu95yoynawFTt0eT4bUq" class="no-margin-top no-margin-bottom no-padding-top no-padding-bottom border-top-no border-bottom-no bg-white txt-default no-bg-image " style="">
+                            <header class="section-header title-dashed title-grey title-left">
+                                <h2 id="region-Lu95yoynawFTt0eT4bUq">DUT (Bac+2), accessibles après le Bac : </h2>
+                            </header>
+                            <div id="row-Z3xWdrdKaduG6Y9YnYR8" class="row row3col row-33-33-33 rowclassic no-margin-top no-margin-bottom no-padding-top no-padding-bottom next-to-sidebar ">
+                                <div class="col">
+                                    <div class="bloc bloc-texte">
+                                        <h3><strong><strong><strong>Genie informatique</strong></strong></strong></h3>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="bloc bloc-texte">
+                                        <h3><strong><strong><strong><strong>technique de management <br/><br/>
+                                                        </strong></strong></strong></strong></h3>
+
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="bloc bloc-texte">
+                                        <h3><strong><strong><strong><strong>genie d'agriculture<br/></strong></strong></strong></strong></h3>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+
+                        <section id="1B526BMrtnpCssAtzliZ" class="margin-top-min no-margin-bottom no-padding-top no-padding-bottom border-top-no border-bottom-no bg-white txt-default no-bg-image " style="">
+                            <header class="section-header title-dashed title-black title-left">
+                                <h2 id="region-1B526BMrtnpCssAtzliZ">Télécharger les plaquettes "DUT"</h2>
+                            </header>
+                            <div id="row-3WTD3hgabtYNqWYIJbHy" class="row row1col row-100 rowclassic no-margin-top no-margin-bottom no-padding-top no-padding-bottom next-to-sidebar ">
+                                <div class="col">
+                                    <div id="accordeon-7449" class="bloc-accordeon">
+                                        <div id="item-1651134772" class="item">
+                                            <div class="titre"><a href="javascript:void(0);" title="Déplier le contenu">Genie informatique<i aria-hidden="true" class="fas fa-plus"></i></a></div>
+                                            <div class="contenu"><p><a href="" target="_blank" rel="noopener">DUT Genie informatique</a></p>
+
+                                            </div>
+                                        </div>
+                                        <div id="item-144031474" class="item">
+                                            <div class="titre"><a href="javascript:void(0);" title="Déplier le contenu">technique de management<i aria-hidden="true" class="fas fa-plus"></i></a></div>
+                                            <div class="contenu"><p><a href="" target="_blank" rel="noopener">DUT technique de management  </a></p>
+                                            </div>
+                                        </div>
+                                        <div id="item-197562688" class="item">
+                                            <div class="titre"><a href="javascript:void(0);" title="Déplier le contenu"> genie d'agriculture <i aria-hidden="true" class="fas fa-plus"></i></a></div>
+                                            <div class="contenu"><p><a href="" target="_blank" rel="noopener">DUT genie d'agriculture </a></p>
+
+
+                                            </div>
+                                        </div>
+
+
+                                </div>
+                            </div>
+                        </section>
+                        <hr class="separator"/>
+                    </div>
+                </div>
 
 
 
-
-            <div class="w3-container">
-                <h3><b><?php the_title(); ?></b></h3>
             </div>
+        </body>
 
-            <div class="w3-container">
-
-
-                It looks like nothing was found at this location. Maybe try a search?
-
-            </div>
-        </div>
-        <hr>
-
-
-        <?php comments_template(); ?>
-
-
-    </div>
-
-
+    </html>
 
 <?php get_footer(); ?>
