b20354d2f5c79e95332d0326a6dd5e9b4e44eefb
mac <aelkhazenti@gmail.com>
2020-03-25 14:34:18 +0100

    add 404 page

---
+++ 404.php
@@ -0,0 +1,33 @@
+<?php get_header(); ?>
+
+
+    <div class="w3-w3-col l8 s12">
+
+
+        <div class="w3-card-4 w3-margin white">
+
+
+
+
+            <div class="w3-container">
+                <h3><b><?php the_title(); ?></b></h3>
+            </div>
+
+            <div class="w3-container">
+
+
+                It looks like nothing was found at this location. Maybe try a search?
+
+            </div>
+        </div>
+        <hr>
+
+
+        <?php comments_template(); ?>
+
+
+    </div>
+
+
+
+<?php get_footer(); ?>
